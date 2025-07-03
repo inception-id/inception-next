b45d69a256a1cf4d2a95585b930b23284c325a26
Stanley Winata <winatastanley355@gmail.com>
2025-07-03 20:01:04 +0700

    refactor: Reset password to email card

---
+++ src/app/(auth)/auth/reset-password/components/email-card.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LuChevronLeft } from "react-icons/lu";
+import { ResetPasswordForm } from "./form";
+
+export const EmailCard = () => {
+  return (
+    <Card className="border-none shadow-none py-0">
+      <CardHeader className="px-0">
+        <CardTitle>Reset Password</CardTitle>
+        <CardDescription>
+          Enter your email below to receive a password reset link
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="px-0">
+        <ResetPasswordForm />
+      </CardContent>
+      <CardFooter className="px-0">
+        <Link
+          href="/auth/login"
+          className="underline underline-offset-4 flex items-center gap-1"
+        >
+          <LuChevronLeft />
+          <span>Back to Login</span>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
--- src/app/(auth)/auth/reset-password/page.tsx
+++ src/app/(auth)/auth/reset-password/page.tsx
@@ -8,33 +8,14 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { LuChevronLeft } from "react-icons/lu";
-import { ResetPasswordForm } from "./components";
+import { EmailCard } from "./components";
 
 const ResetPasswordPage = () => {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm ">
         <h1 className="mb-4 text-2xl">INCEPTION</h1>
-        <Card className="border-none shadow-none py-0">
-          <CardHeader className="px-0">
-            <CardTitle>Reset Password</CardTitle>
-            <CardDescription>
-              Enter your email below to receive a password reset link
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="px-0">
-            <ResetPasswordForm />
-          </CardContent>
-          <CardFooter className="px-0">
-            <Link
-              href="/auth/login"
-              className="underline underline-offset-4 flex items-center gap-1"
-            >
-              <LuChevronLeft />
-              <span>Back to Login</span>
-            </Link>
-          </CardFooter>
-        </Card>
+        <EmailCard />
       </div>
     </div>
   );
