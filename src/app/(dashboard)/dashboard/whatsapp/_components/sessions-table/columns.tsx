6349cf21b4de3b8155c0fec76b1b512abc0cdc51
Stanley Winata <winatastanley355@gmail.com>
2025-08-10 10:42:08 +0700

    id columns

--- src/app/(dashboard)/dashboard/whatsapp/_components/sessions-table/columns.tsx
+++ src/app/(dashboard)/dashboard/whatsapp/_components/sessions-table/columns.tsx
@@ -1,8 +1,42 @@
 import { WhatsappSession } from "@/lib/api/whatsapp/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { DeleteDialog } from "./delete-dialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { LuEye } from "react-icons/lu";
+import { toast } from "sonner";
 
 export const TABLE_COLUMNS: ColumnDef<WhatsappSession>[] = [
+  {
+    header: "id",
+    accessorKey: "id",
+    cell: ({ row }) => {
+      const [show, setShow] = useState(false);
+      return (
+        <div className="flex items-center border rounded-md w-fit">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="rounded-r-none"
+            onClick={() => setShow(!show)}
+          >
+            <LuEye />
+          </Button>
+          <Input
+            className="border-none rounded-l-none w-fit"
+            type={show ? "text" : "password"}
+            value={row.original.id}
+            readOnly
+            onClick={async () => {
+              await navigator.clipboard.writeText(row.original.id);
+              toast.info("ID copied to clipboard");
+            }}
+          />
+        </div>
+      );
+    },
+  },
   {
     header: "Phone Number",
     accessorKey: "phone",
@@ -25,7 +59,7 @@ export const TABLE_COLUMNS: ColumnDef<WhatsappSession>[] = [
   },
   {
     header: "",
-    accessorKey: "id",
+    accessorKey: "is_ready",
     cell: ({ row }) => {
       return <DeleteDialog session={row.original} />;
     },
