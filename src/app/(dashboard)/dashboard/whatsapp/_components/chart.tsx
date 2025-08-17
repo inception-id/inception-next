9afb9b35b62aee083416bdbae7b5ef6acc11e672
Stanley Winata <winatastanley355@gmail.com>
2025-08-17 10:23:29 +0700

    Dashboard

--- src/app/(dashboard)/dashboard/whatsapp/_components/chart.tsx
+++ src/app/(dashboard)/dashboard/whatsapp/_components/chart.tsx
@@ -29,12 +29,15 @@ const CustomTooltip = ({
   const isVisible = active && payload && payload.length;
   if (isVisible) {
     return (
-      <div className="bg-white rounded p-2 text-sm">
+      <div className="bg-white rounded p-2 text-sm dark:text-background">
         <div className="font-semibold">
           {getMonthShortName(payload[0].payload.month)}{" "}
           {payload[0].payload.year}
         </div>
-        <div>{payload[0].value} messages</div>
+        <div className="flex items-center gap-1">
+          <span className="bg-chart-2 w-2 h-2 rounded" />
+          <span>{payload[0].value} messages</span>
+        </div>
       </div>
     );
   }
@@ -56,7 +59,7 @@ export const Chart = ({ chartData }: ChartProps) => {
           />
           <YAxis
             dataKey="total_records"
-            width={15}
+            width={20}
             className="text-xs"
             tickLine={false}
           />
--- src/app/(dashboard)/dashboard/whatsapp/_components/dashboard.tsx
+++ src/app/(dashboard)/dashboard/whatsapp/_components/dashboard.tsx
@@ -4,6 +4,7 @@ import {
 } from "@/lib/api/whatsapp/client";
 import { MdMessage } from "react-icons/md";
 import { Chart } from "./chart";
+import { MessageCountTable } from "./table";
 
 type DashboardProps = {
   environment: WhatsappMessageType;
@@ -13,8 +14,9 @@ export const Dashboard = async ({ environment }: DashboardProps) => {
   const messageCount = await findWhatsappMessagesAllTimeCount(environment);
 
   return (
-    <div>
+    <div className="flex flex-col gap-4 lg:flex-row">
       <Chart chartData={messageCount.data} />
+      <MessageCountTable data={messageCount.data} />
     </div>
   );
 };
