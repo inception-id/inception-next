37a26cb578c71b664309a5678a00def4a06e804a
Stanley Winata <winatastanley355@gmail.com>
2025-09-25 11:19:19 +0700

    toc

--- src/app/(whatsapp)/whatsapp/documentation/_components/toc.tsx
+++ src/app/(whatsapp)/whatsapp/documentation/_components/toc.tsx
@@ -1,43 +1,104 @@
 "use client";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export const TableOfContent = () => {
   const router = useRouter();
   return (
-    <Card className="gap-2 max-w-md h-fit">
+    <Card className="w-full max-w-sm h-fit gap-0">
       <CardHeader>
         <CardTitle>Table of Contents</CardTitle>
       </CardHeader>
       <CardContent>
-        <div
-          role="button"
-          className="py-2 w-full underline hover:text-blue-700 cursor-pointer"
-          onClick={() => router.push("#login")}
+        <Accordion type="multiple">
+          <AccordionItem value="auth">
+            <AccordionTrigger>1. Authentication</AccordionTrigger>
+            <AccordionContent className="flex flex-col">
+              <Link
+                href="#auth-login"
+                className={cn(
+                  buttonVariants({ variant: "link" }),
+                  "justify-start",
+                )}
+              >
+                1.1 Login/Register
+              </Link>
+              <Link
+                href="#auth-key"
+                className={cn(
+                  buttonVariants({ variant: "link" }),
+                  "justify-start",
+                )}
+              >
+                1.2 API Key
+              </Link>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="notification">
+            <AccordionTrigger>2. Send via Inception number</AccordionTrigger>
+            <AccordionContent className="flex flex-col">
+              <Link
+                href="notification-single"
+                className={cn(
+                  buttonVariants({ variant: "link" }),
+                  "justify-start",
+                )}
+              >
+                2.1 Send one message/notification
+              </Link>
+              <Link
+                href="#notification-multiple"
+                className={cn(
+                  buttonVariants({ variant: "link" }),
+                  "justify-start",
+                )}
+              >
+                2.2 Send multiple messages/notifications
+              </Link>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="message">
+            <AccordionTrigger>3. Send with your number</AccordionTrigger>
+            <AccordionContent className="flex flex-col">
+              <Link
+                href="message-single"
+                className={cn(
+                  buttonVariants({ variant: "link" }),
+                  "justify-start",
+                )}
+              >
+                3.1 Send one message/notification
+              </Link>
+              <Link
+                href="message-multiple"
+                className={cn(
+                  buttonVariants({ variant: "link" }),
+                  "justify-start",
+                )}
+              >
+                3.2 Send multiple messages/notifications
+              </Link>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Link
+          href="#faq"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "justify-start px-0 w-full",
+          )}
         >
-          1. Login/Register
-        </div>
-        <div
-          role="button"
-          className="py-2 w-full underline hover:text-blue-700 cursor-pointer"
-          onClick={() => router.push("#key")}
-        >
-          2. Create API Key
-        </div>
-        <div
-          role="button"
-          className="py-2 w-full underline hover:text-blue-700 cursor-pointer"
-          onClick={() => router.push("#wa-inception")}
-        >
-          3. Sending message/notification via Inception number
-        </div>
-        <div
-          role="button"
-          className="py-2 w-full underline hover:text-blue-700 cursor-pointer"
-          onClick={() => router.push("#wa-self")}
-        >
-          4. Sending message/notification with your number
-        </div>
+          4. FAQ
+        </Link>
       </CardContent>
     </Card>
   );
--- src/app/(whatsapp)/whatsapp/documentation/page.tsx
+++ src/app/(whatsapp)/whatsapp/documentation/page.tsx
@@ -4,10 +4,10 @@ const WhatsappDocumentation = () => {
   return (
     <div className="container mx-auto p-4 flex flex-col lg:flex-row gap-8">
       <TableOfContent />
-      <div className="flex flex-col gap-6">
+      {/*<div className="flex flex-col gap-6">
         <Content />
         <Faq />
-      </div>
+      </div>*/}
     </div>
   );
 };
